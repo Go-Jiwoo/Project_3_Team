17c246b3c91cd126ac5ec978eea83e9f41d77da8
Go-Jiwoo <giwoolove@naver.com>
2023-08-25 17:35:49 +0900

    230825-1

--- js/script.js
+++ js/script.js
@@ -1,27 +1,43 @@
-const content = document.querySelectorAll('.menu');
-const subContent = document.querySelectorAll('.board')
-const ACTIVE = "active"
+function animateValue(id, start, end, duration) {
+    const obj = document.getElementById(id);
+    const stepTime = Math.abs(Math.floor(duration / (end - start)));
+    
+    let current = start;
+    const step = end > start ? 1 : -1;
+    
+    const timer = setInterval(function() {
+        current += step;
+        obj.innerHTML = current;
+        
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+const target = document.getElementById('number').getAttribute('data-target');
+// animateValue('number', 600, parseInt(target), 1000);
 
 
-content.forEach(function(contents){
-    contents.addEventListener("click",function(){
 
-        subContent.forEach(function(subContents){
-            subContents.classList.remove(ACTIVE);
-        })
-        content.forEach(function(contents){
-            contents.classList.remove(ACTIVE);
-        });
-        contents.classList.add(ACTIVE);
-    })
-    contents.classList.remove(ACTIVE);
+    window.addEventListener("scroll",function(){
+    if(window.scrollY>450){
+        animateValue('number', 600, parseInt(target), 1000);
+    }
 })
 
-subContent.forEach(function(subContents){
-    subContents.addEventListener("click",function(){
-        subContent.forEach(function(subContents){
-            subContents.classList.remove(ACTIVE);
-        })
-        subContents.classList.add(ACTIVE);
-    })
-})
+
+$(document).ready(function(){
+
+
+
+    
+    $(".section li").click(function(){
+        let a = $(this).index();
+        console.log(a);
+        $(".Page:nth-of-type(4) .section .board").eq(a).css("display","block");
+        $(".Page:nth-of-type(4) .section .board").eq(a).siblings(".board").css("display","none");
+        $(".section li").css({"font-weight":"200","opacity":"0.8"});
+        $(this).css({"font-weight":"600","opacity":"1"});
+    });
+});
