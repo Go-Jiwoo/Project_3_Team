d7bbe246b9128d98a0821c13ef918e54f45e813c
Go-Jiwoo <giwoolove@naver.com>
2023-08-28 17:38:36 +0900

    230828-1

--- js/script.js
+++ js/script.js
@@ -18,26 +18,22 @@ function animateValue(id, start, end, duration) {
 const target = document.getElementById('number').getAttribute('data-target');
 // animateValue('number', 600, parseInt(target), 1000);
 
-
-
-    window.addEventListener("scroll",function(){
-    if(window.scrollY>450){
-        animateValue('number', 600, parseInt(target), 1000);
-    }
-})
+window.addEventListener("scroll",function(){
+if(window.scrollY>450){
+    animateValue('number', 600, parseInt(target), 1000);
+}
+},{once:true})
 
 
 $(document).ready(function(){
 
-
-
-    
     $(".section li").click(function(){
         let a = $(this).index();
         console.log(a);
-        $(".Page:nth-of-type(4) .section .board").eq(a).css("display","block");
+        $(".Page:nth-of-type(4) .section .board").eq(a).css("display","flex");
         $(".Page:nth-of-type(4) .section .board").eq(a).siblings(".board").css("display","none");
         $(".section li").css({"font-weight":"200","opacity":"0.8"});
         $(this).css({"font-weight":"600","opacity":"1"});
     });
+    
 });
